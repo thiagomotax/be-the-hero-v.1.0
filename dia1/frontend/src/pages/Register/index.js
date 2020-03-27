d7fe37cc63ef58f519dc22f71612ae08bd4fdb78
Thiago Mota <thiagomotax@gmail.com>
2020-03-27 17:51:00 -0300

    integração back-front ok no cadastro de ongs

--- dia1/frontend/src/pages/Register/index.js
+++ dia1/frontend/src/pages/Register/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './styles.css';
 import logoImg from '../../assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi'; 
+import api from '../../services/api';
 
 export default function Register(){
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [whatsapp, setWhatsapp] = useState('');
+    const [city, setCity] = useState('');
+    const [uf, setUf] = useState('');
+
+
+    async function handleRegister (e){ //disparada quando o formulário recebe ação de submit
+        
+        e.preventDefault(); //prevenir a página de atualizar(comportamento padrão)
+
+        //buscar dados dos inputs e enviar pra api  //console.log(name, whatsapp);
+        const data = {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        }
+        try{
+            const response = await api.post('ongs', data); //axios ja envia em json
+            alert("Seu id de acesso: " + response.data.id);
+        }
+        catch(err){
+            alert('Erro no cadastro' + err);
+        }
+        
+    }
     return (
         <div className="register-container">
             <div className="content">
@@ -17,13 +46,28 @@ export default function Register(){
                         Não tenho cadastro
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Nome da Ong" />
-                    <input type="email" placeholder="E-mail" />
-                    <input placeholder="Whatsapp" />
+                <form onSubmit={handleRegister}>
+                    <input placeholder="Nome da Ong" 
+                    value={name}
+                    onChange={e => setName(e.target.value)}/> {/*recebe e e depois atualiza a função com o valor do e*/}
+
+                    <input type="email" placeholder="E-mail"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)} />
+
+                    <input placeholder="Whatsapp" 
+                    value={whatsapp}
+                    onChange={e => setWhatsapp(e.target.value)}/>
+
                     <div className="input-group">
-                        <input placeholder="Cidade" />
-                        <input placeholder="UF" style={ {width: 80} }/>
+                        <input placeholder="Cidade" 
+                        value={city}
+                        onChange={e => setCity(e.target.value)}/>
+
+                        <input placeholder="UF"
+                        value={uf}
+                        onChange={e => setUf(e.target.value)} 
+                        style={ {width: 80} }/>
                     </div>
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
