2b5fa741445b4fbcc87b7ef8515229278738f440
Thiago Mota <thiagomotax@gmail.com>
2020-03-28 15:29:42 -0300

    bug no envio de email

--- dia1/mobile/src/pages/detail/index.js
+++ dia1/mobile/src/pages/detail/index.js
@@ -3,11 +3,24 @@ import {Feather} from '@expo/vector-icons';
 import {View, Image, Text, TouchableOpacity} from 'react-native';
 import logoImg from '../../assets/logo.png'; //pega a melhor logo, @3x, @2x etc
 import {useNavigation} from '@react-navigation/native';
-
+//import * as MailComposer from 'expo-mail-composer';
 import styles from './styles';
+import {Linking} from 'react-native';
 
 export default function Detail(){
     const navigation = useNavigation();
+    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "cadelinha atropelada" com o valor de R$ 120,00';
+
+    // function sendMail(){
+    //     MailComposer.composeAsync({
+    //         subject: 'Herói do caso: cadelinha atropelada',
+    //         recipients: ['thiagomotax@gmail.com'],
+    //         body: message
+    //     })
+    // }
+    function sendWhatsapp(){
+        Linking.openURL('whatsapp://send?phone=+5532984805202&text='+message);
+    }
 
     function navigateBack(){
         navigation.goBack();
@@ -39,7 +52,7 @@ export default function Detail(){
                 <Text style={styles.heroDescription}>Entre em contato:</Text>
 
                 <View style={styles.actions}>
-                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
                         <Text style={styles.actionText}>Whatsapp</Text>
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.action} onPress={() => {}}>
--- dia1/mobile/src/pages/incidents/index.js
+++ dia1/mobile/src/pages/incidents/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState } from 'react';
 import {View, FlatList, Image, Text, TouchableOpacity} from 'react-native';
 import styles from './styles';
 import logoImg from '../../assets/logo.png'; //pega a melhor logo, @3x, @2x etc
 import { Feather } from '@expo/vector-icons';
 import {useNavigation} from '@react-navigation/native';
+import api from '../../services/api';
+
 export default function Incident(){
+    const [incidents, setIncidents] = useState([]);
     const navigation = useNavigation();
 
     function navigateToDetail(){
         navigation.navigate('Detail');
     }
+
+    async function loadIncidents(){
+        const response = await api.get('incidents');
+
+        setIncidents(response.data);
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
 
     return ( 
         <View style={styles.container}>
@@ -24,19 +37,19 @@ export default function Incident(){
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia</Text>
             <FlatList 
                 style={styles.incidentsList}
-                data={[1,2,3,4,5]}
-                keyExtractor={incident => String(incident)}
+                data={incidents}
+                keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                renderItem={({ item:incident}) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>Ong:</Text>
-                        <Text style={styles.incidentValue}>APAD</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>Caso:</Text>
-                        <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>Valor:</Text>
-                        <Text style={styles.incidentValue}>R$ 120,0</Text>
+                        <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL' }).format(incident.value)}</Text>
 
                         <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
