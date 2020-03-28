a15ad23ab465c3a11f9464f45bf45e0f3d2519ee
Thiago Mota <thiagomotax@gmail.com>
2020-03-28 13:51:20 -0300

    savepoint, bye fucking Windows

---
+++ dia1/mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Open up App.js to awday working on your app!</Text>
+    </View>
+  );
+}
+
